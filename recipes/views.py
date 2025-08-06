8c2594457ab2fcea7891b91301acc5adecc4a9d7
letsrosa <leticiarosasouza6304@gmail.com>
2025-08-05 21:35:08 -0300

    criacao de recipe_detail onde mostra detalhes de cada receita

--- recipes/views.py
+++ recipes/views.py
@@ -5,7 +5,7 @@ def recipe_list(request):
     recipes = Recipe.objects.all()
     return render(request, 'recipes/recipe_list.html', {'recipes': recipes})
 
+
+def recipe_detail(request, pk):
     recipe = get_object_or_404(Recipe, pk=pk)
     return render (request, 'recipes/recipe_detail.html', {'recipe': recipe})
-
-
